e196fe3dd0e40f6bf14fdff63d708236e2dba09e
EvanJason <EvanJason@users.noreply.github.com>
2021-10-03 07:24:29 +0000

    deploy: 39a7d17666e7cd8322f93a118db677daaf4eb822

--- categories/index_props.js
+++ categories/index_props.js
@@ -22,7 +22,7 @@ export default {
     'contributors': [
         "EvanJason"
     ],
-    'date': "2021-10-03T06:30:39.000Z",
+    'date': "2021-10-03T07:22:26.000Z",
     'updated': null,
     'excerpt': "",
     'cover': undefined,
